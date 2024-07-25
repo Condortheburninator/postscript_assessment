9d44cab0f45bae1fbd7e8e872b605759872ecb3b
Conner <conner@conners-mbp.gateway.pace.com>
2024-07-25 15:29:24 -0400

    built this out;

--- analysis/2.eda.sql
+++ analysis/2.eda.sql
@@ -1,4 +1,125 @@
 
 
-SUMMARIZE SMS_PLATFORM_A_SUBSCRIBER_LIST ;
-SUMMARIZE SMS_PLATFORM_B_SUBSCRIBER_LIST ;
+-- SUMMARIZE SMS_PLATFORM_A_SUBSCRIBER_LIST ;
+-- SUMMARIZE SMS_PLATFORM_B_SUBSCRIBER_LIST ;
+
+-- list a
+
+    -- check the length of the phone numbers
+
+        WITH
+
+        PHONE_COUNTS AS (
+
+            SELECT
+                     LEN( CAST( PHONE_NUMBER AS VARCHAR ) ) AS PHONE_NUMBER
+                    --  PHONE_NUMBER
+                    ,COUNT( DISTINCT PHONE_NUMBER )         AS PHONE_COUNT
+
+            FROM
+                    SUBSCRIBER_LIST_A
+                    -- SUBSCRIBER_LIST_B
+
+            WHERE
+                    1 = 1
+                    AND PHONE_NUMBER IS NOT NULL
+
+            GROUP BY
+                    ALL
+
+            ORDER BY
+                    PHONE_NUMBER
+
+        )
+
+        SELECT
+                 *
+                ,PHONE_COUNT / SUM( PHONE_COUNT ) OVER () * 100 AS 'PER_OF_TOTAL'
+
+        FROM
+                PHONE_COUNTS
+
+        ;
+
+    -- for phone numbers with 9 digits, make sure they start with 1
+
+        WITH
+
+        PHONE_COUNTS AS (
+
+            SELECT
+                    --  PHONE_NUMBER
+                     LEFT(CAST( PHONE_NUMBER AS VARCHAR ) , 1)  AS LEADING_DIGIT
+                    ,COUNT( DISTINCT PHONE_NUMBER )             AS PHONE_COUNT
+
+            FROM
+                    SUBSCRIBER_LIST_A
+
+            WHERE
+                    1 = 1
+                    AND LEN(CAST( PHONE_NUMBER AS VARCHAR )) = 9
+                    -- AND LEFT(CAST( PHONE_NUMBER AS VARCHAR ) , 1) != 1
+
+            GROUP BY
+                    ALL
+
+        )
+
+        SELECT
+                *
+                ,PHONE_COUNT / SUM( PHONE_COUNT ) OVER() * 100  AS PER_OF_TOTAL
+
+        FROM
+                PHONE_COUNTS
+
+        WHERE
+                1 = 1
+
+        ORDER BY
+                LEADING_DIGIT
+        ;
+
+    -- opt-in source
+
+    WITH
+
+    SOURCE AS (
+
+    SELECT
+         OPT_IN_SOURCE
+        ,COUNT(DISTINCT PHONE_NUMBER)   AS PHONE_COUNT
+
+    FROM
+            SUBSCRIBER_LIST_A
+
+    GROUP BY
+            ALL
+
+    )
+
+    SELECT
+            *
+            , PHONE_COUNT / SUM( PHONE_COUNT ) OVER () * 100    AS PER_OF_TOTAL
+
+    FROM
+            SOURCE
+
+    ;
+
+
+---
+
+SELECT
+         STATUS
+        ,COUNT( DISTINCT PHONE_NUMBER   )   AS STATUS_COUNT
+
+FROM
+        SUBSCRIBER_LIST_A
+
+WHERE
+        1 = 1
+
+GROUP BY
+        ALL
+
+;
