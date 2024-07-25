b19f1fa45cb8990344ecadc2f73502a96914e43e
Conner <conner@conners-mbp.gateway.pace.com>
2024-07-25 11:46:28 -0400

    updated code here

--- analysis/1.load_data.sql
+++ analysis/1.load_data.sql
@@ -12,16 +12,20 @@ AS
         ,null_padding   = TRUE
         -- ,all_varchar    = TRUE
         ,ignore_errors  = TRUE
-        ,columns = {
+        -- ,columns = {
 
-            'Phone Number'          : BIGINT
-            ,'Opt-in Source'        : VARCHAR
-            ,'Date added'           : TIMESTAMP
-            ,'Status'               : VARCHAR
-            ,'Date unsubscribed'    : VARCHAR
-        }
+        --     'Phone Number'          : BIGINT
+        --     ,'Opt-in Source'        : VARCHAR
+        --     ,'Date added'           : TIMESTAMP
+        --     ,'Status'               : VARCHAR
+        --     ,'Date unsubscribed'    : VARCHAR
+        -- }
 
     );
 
+    -- SELECT * FROM SMS_PLATFORM_A_SUBSCRIBER_LIST LIMIT 1000 ;
+    -- DROP TABLE SMS_PLATFORM_A_SUBSCRIBER_LIST ;
 
--- CREATE TABLE SMS_PLATFORM_B_SUBSCRIBER_LIST AS SELECT * FROM 'data/SMS Platform B Subscriber List.csv';
+CREATE TABLE SMS_PLATFORM_B_SUBSCRIBER_LIST AS SELECT * FROM 'data/SMS Platform B Subscriber List.csv';
+
+-- SHOW TABLES ;
